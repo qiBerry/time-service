b0481be2922f68eba5887017387ee93a03a77179
Tkachenko Nikita Sergeevich <pspnik84@gmail.com>
2021-04-22 00:54:14 +0300

    pushed like a python library

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='time_service',
+    packages=find_packages(include=['time_service']),
+    version='0.1',
+    description='Yandex Test Task',
+    author='ME',
+    license='MIT',
+)
